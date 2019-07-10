de93f661c2f41f6e9cb5d8b233cd0cac8528b0b0
Portno <j.remoundos@dotbydot.gr>
2019-07-10 18:01:10 +0300

    Update CmdManager.cs

--- src/ServerCommunicator/CmdManager.cs
+++ src/ServerCommunicator/CmdManager.cs
@@ -6,9 +6,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
-using PanaceaLib;
-using SocketIOClient;
-using SocketIOClient.Messages;
+
 
 namespace ServerCommunicator
 {
